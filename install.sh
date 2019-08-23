477be55c4b30c651fba110a4bd28a60c4f58ccbb
Damian ONeill <damian.oneill@me.com>
2019-08-23 15:11:48 +0100

    few extras

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ fi
 
 
 brew tap caskroom/cask
-brew install ag autojump bash-git-prompt cloc ctags ctop curl derailed/k9s/k9s dos2unix fasd git git-extras git-flow go hub htop httpie kubectl kubernetes-cli net-snmp nmap node nvm openssl pass rpm ssh-copy-id the_silver_searcher tig tiff2png tmux tree vim wget http://git.io/sshpass.rb
+brew install ag autojump bash-git-prompt cloc ctags ctop curl derailed/k9s/k9s dos2unix fasd git git-extras git-flow go helmfile hub htop httpie kubectl kubernetes-cli net-snmp nmap node nvm openssl pass rpm ssh-copy-id the_silver_searcher tig tiff2png tmux tree vim wget http://git.io/sshpass.rb
 brew cask install chicken cyberduck docker dropbox balenaetcher firefox google-chrome iterm2 java kindle skype slack spotify teamviewer vagrant visual-studio-code whatsapp opera virtualbox
 
 # add completions for the above applications
