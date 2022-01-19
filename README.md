1f74e8fee414ed3febdd138f602f2c740ea0df41
Damian ONeill <>
2022-01-19 13:35:01 +0000

    first cut of zsh solution

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # mac-setup
 
-This solution will install homebrew and then use brew to install the depdenencies.  At various points homebrew may ask you for your password. 
+This solution will install homebrew and then use brew to install the dependencies. At various point's homebrew may ask you for your password.
 
-Note at the end Virtualbox will fail, as it needs permissions granted for oracle before installing the software, it will provide the location of the change you require.  Follow the instructions, then run the script again.
+It defaults to zsh and installs oh my zsh if not present.
+
+[asdf](https://github.com/asdf-vm/asdf) is used as the version manager, it installs the following language plugins and some default versions for each language:
+
+- Java
+- Golang
+- Node
+- Python
+
+Note at the end Virtualbox may fail, if it needs permissions granted for oracle before installing the software, it will provide the location of the change you require. Follow the instructions, then run the script again.
 
 ```sh
 ./install.sh
--- install.sh
+++ install.sh
@@ -1,53 +1,140 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v brew)" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# make sure profile exists
-touch ~/.bash_profile
+shell=zsh
+while true; do
+  read -r -p '>>> What shell are you using? bash or [zsh]: ' answer
+  answer=${answer:-zsh}
+  case "$answer" in
+  zsh)
+    shell=zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    grep -qxF '. /usr/local/opt/asdf/libexec/asdf.sh' ~/.zshrc || echo '. /usr/local/opt/asdf/libexec/asdf.sh' >>~/.zshrc
+    break
+    ;;
+  bash)
+    shell=bash
+    touch ~/.bashrc
+    touch ~/.bash_profile
+    grep -qxF '. /usr/local/opt/asdf/libexec/asdf.sh' ~/.bash_profile || echo '. /usr/local/opt/asdf/libexec/asdf.sh' >>~/.bash_profile
+    break
+    ;;
+  *)
+    printf "%s\n" 'Answer either “zsh” or “bash”.'
+    ;;
+  esac
+done
 
-# upgrade bash version && add completion
-grep -qxF '/usr/local/bin/bash' /etc/shells
-if [ $? -ne 0 ]; then
-  brew install bash
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
-  chsh -s /usr/local/bin/bash
+echo ">>> Configured for $shell"
+
+# declare -a taps=(
+
+# )
+
+declare -a terminal=(
+  "zsh"       # UNIX shell (command interpreter) - https://www.zsh.org/
+  "alacritty" # terminal emulator - https://github.com/alacritty/alacritty
+  "tmux"      # terminal multiplexer - https://github.com/tmux/tmux
+  "neovim"    # Vim-based text editor - https://neovim.io/
+)
+
+declare -a toolsAlternative=(
+  "lsd"  # instead of ls - https://github.com/Peltoche/lsd
+  "bat"  # instead of cat - https://github.com/sharkdp/bat
+  "fd"   # instead of find - https://github.com/sharkdp/fd
+  "rg"   # instead of grep - https://github.com/BurntSushi/ripgrep
+  "htop" # instead of top - https://htop.dev/
+)
+
+declare -a productivity=(
+  "topgrade"        # system updater - https://github.com/r-darwish/topgrade
+  "asdf"            # software tools version manager - https://github.com/asdf-vm/asdf
+  "cloc"            # count lines of code - https://github.com/AlDanial/cloc
+  "universal-ctags" # maintained implementation of ctags - https://github.com/universal-ctags/ctags
+  "ctop"            # Top-like interface for container metrics - https://github.com/bcicen/ctop
+  "curl"            # Get a file from an HTTP, HTTPS or FTP server - https://curl.se
+  "dos2unix"        # Convert text between DOS, UNIX, and Mac formats - https://waterlan.home.xs4all.nl/dos2unix.html
+  "git"             # Distributed revision control system - https://git-scm.com
+  "git-extras"      # Small git utilities - https://github.com/tj/git-extras
+  "nmap"            # Port scanning utility - https://nmap.org/
+  "pass"            # Password manager - https://www.passwordstore.org/
+  "tree"            # Display directories as trees - http://mama.indstate.edu/users/ice/tree/
+  "wget"            # Internet file retriever - https://www.gnu.org/software/wget/
+  "xquartz"         # Open-source version of the X.Org X Window System - https://www.xquartz.org/
+)
+
+# most kubernetes tools are versioned using asdf, see below.
+declare -a kubernetes=(
+  "k9s" # Kubernetes CLI To Manage Clusters - https://k9scli.io/
+)
+
+declare -a guiApps=(
+  "dropbox"            # Client for the Dropbox cloud storage service - https://www.dropbox.com/
+  "docker"             # App to build and share containerized applications and microservices - https://www.docker.com/products/docker-desktop
+  "firefox"            # Web browser - https://www.mozilla.org/firefox/
+  "google-chrome"      # Web browser - https://www.google.com/chrome/
+  "kindle"             # Interface for reading and syncing eBooks - https://www.amazon.com/gp/digital/fiona/kcp-landing-page
+  "skype"              # Video chat, voice call and instant messaging application - https://www.skype.com/
+  "slack"              # Team communication and collaboration software - https://slack.com/
+  "spotify"            # Music streaming service - https://www.spotify.com/
+  "teamviewer"         # Remote access and connectivity software focused on security - https://www.teamviewer.com/
+  "visual-studio-code" # Open-source code editor - https://code.visualstudio.com/
+  "whatsapp"           # Desktop client for WhatsApp - https://www.whatsapp.com/
+  "virtualbox"         # Virtualizer for x86 hardware - https://www.virtualbox.org/
+)
+
+# brew tap "${taps[@]}"
+brew install --no-quarantine "${terminal[@]}"
+brew install --no-quarantine "${toolsAlternative[@]}"
+brew install --no-quarantine "${productivity[@]}"
+brew install --no-quarantine "${kubernetes[@]}"
+brew install --no-quarantine "${guiApps[@]}"
+
+. /usr/local/opt/asdf/libexec/asdf.sh
+
+JDK_VER=adoptopenjdk-openj9-8.0.292+10.openj9-0.26.0
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf install java $JDK_VER
+asdf global java $JDK_VER
+
+GO_VER=1.17.6
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf install golang $GO_VER
+asdf global golang $GO_VER
+
+NODE_VER=lts-fermium
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs $NODE_VER
+asdf global nodejs $NODE_VER
+
+PYTHON_VER=3.10.2
+asdf plugin-add python https://github.com/danhper/asdf-python.git
+asdf install python $PYTHON_VER
+asdf global python $PYTHON_VER
+
+KUBECTL_VER=1.23.1
+asdf plugin-add kubectl https://github.com/Banno/asdf-kubectl.git
+asdf install kubectl $KUBECTL_VER
+asdf global kubectl $KUBECTL_VER
+
+# install vs code extensions
+declare -a vscodeExts=(
+  "ms-vscode-remote.remote-ssh" # Remote - use any remote machine with a SSH server as your development environment - https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-ssh
+)
+code --install-extension "${vscodeExts[@]}"
+
+FONTS_DIR="$HOME/Library/Fonts"
+if [ ! -d "$FONTS_DIR" ]; then
+  echo ">>> Installing powerline fonts in ${FONTS_DIR} useful for on my zsh themes"
+  git clone https://github.com/powerline/fonts.git --depth=1
+  cd fonts || exit
+  ./install.sh
+  cd ..
+  rm -rf fonts
 fi
-brew install bash-completion 
-grep -qxF '[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"'  ~/.bash_profile
-if [ $? -ne 0 ]; then
-cat <<EOT >> ~/.bash_profile
-[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
-EOT
-fi
 
-
-brew install ag autojump bash-git-prompt cloc ctags ctop curl derailed/k9s/k9s dos2unix fasd git git-extras git-flow go helmfile hub htop httpie kubectl kubernetes-cli net-snmp nmap node nvm openssl pass pyenv rpm socat ssh-copy-id the_silver_searcher tig tiff2png tmux tree vim wget xquartz
-brew install chicken cyberduck docker dropbox balenaetcher firefox google-chrome iterm2 java kindle skype slack spotify teamviewer vagrant visual-studio-code whatsapp opera virtualbox
-
-# add completions for the above applications
-grep -qxF 'source <(kubectl completion bash)'  ~/.bash_profile
-if [ $? -ne 0 ]; then
-cat <<EOT >> ~/.bash_profile
-source <(kubectl completion bash)
-if [ -f "/usr/local/opt/bash-git-prompt/share/gitprompt.sh" ]; then
-    __GIT_PROMPT_DIR="/usr/local/opt/bash-git-prompt/share"
-    source "/usr/local/opt/bash-git-prompt/share/gitprompt.sh"
-fi
-[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
-EOT
-fi
-
-grep -qxF 'export PYENV_ROOT='  ~/.bash_profile
-if [ $? -ne 0 ]; then
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/shims:$PATH"' >> ~/.bash_profile
-fi
-
-npm install --global git-open
-
-code --install-extension ms-vscode-remote.remote-ssh
+echo ">>> Dry run of topgrade, checking to see if software needs updated"
+topgrade -n
